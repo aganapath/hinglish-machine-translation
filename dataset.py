726915a452ec8ba7423287b92361042bb77dbb1d
anjaniganapathy <anjani.ganapathy@gmail.com>
2025-04-17 14:28:54 +0100

    Updated custom Dataset class to process and prepare hinglish / english sentences for model input

--- dataset.py
+++ dataset.py
@@ -1,63 +1,69 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
 
-class BertHingDataset(Dataset):
-  def __init__(self, sequence, tokenizer, max_length = 512):
-    self.sequence = sequence
-    self.tokenizer = tokenizer
+from torch.utils.data import Dataset, DataLoader
+from transformers import PreTrainedTokenizer
+
+# create a dataset class that will do the following:
+# 1. load the text file and tokenize the sequences of words
+# 2. make the output of the tokenized inputs consistent with the input words (in case they were split up during tokenization)
+# 3. create a label2index thingy that is consistent with the labels of the original words
+# 4. lastly generate the tensors for inputs and labels that will get passed into the model
+# BERT model wants 1) input_ids - sequence of indices that correspond to each word's POS tag
+#                  2) attention_mask - will output from tokenizer
+#                  3) labels - sequence (list) of
+
+class HingDataset(Dataset):
+  def __init__(self, word_to_indices, in_sequences, out_sequences, max_length = 30):
+    self.word_to_indices = word_to_indices
+    self.in_sequences = in_sequences
+    self.out_sequences = out_sequences
+    # self.tokenizer = tokenizer
+    # self.label2index = label2index
     self.max_length = max_length
-    self.pad_token_id = tokenizer.pad_token_id #what does this do?
-    self.pad_label_id = -100 #for
+    # self.pad_token_id = tokenizer.pad_token_id #what does this do?
+    # self.pad_label_id = -100 #for
+
+  def __len__(self):
+    return len(self.in_sequences)
 
   def __getitem__(self, idx):
+    input_tokens = []
+    self.in_sequence = self.in_sequences[idx].lower().split()
+    self.out_sequence = self.out_sequences[idx].lower().split()
 
-    # this is a shorthand for creating two lists
-    # of tokens and tags for all the tuples in a
-    # specific sentence
-    # words, tags = zip(*self.sequence[idx])
+    for w in self.in_sequence:
+      w_str = w.strip(',')
+      if w_str in self.word_to_indices['hi']:
+        input_tokens.append(self.word_to_indices['hi'][w_str])
+      elif w_str in self.word_to_indices['en']:
+        input_tokens.append(self.word_to_indices['en'][w_str])
+      else:
+        input_tokens.append(self.word_to_indices['<unk>'])
 
-    # tokens, tags = zip(*self.sentences[idx])
-    # tokenized_sentences = []
-    # sentences_lang_id = []
+    output_tokens = []
+    for w in self.out_sequence:
+      w_str = w.strip('.,!?:\'').replace('\'', '')
+      if w_str in self.word_to_indices['en']:
+        output_tokens.append(self.word_to_indices['en'][w_str])
+      else:
+        output_tokens.append(self.word_to_indices['<unk>'])
 
-    tokenized_input = self.tokenizer(self.sequence,
-                                    is_split_into_words=True,
-                                    padding='max_length',
-                                    truncation=True,
-                                    max_length=self.max_length,
-                                    return_tensors='pt')
+    # pad and truncate
+    if len(input_tokens) < self.max_length:
+      diff = self.max_length - len(input_tokens)
+      padding = [self.word_to_indices['<pad>']] * diff
+      input_tokens = padding + input_tokens
 
-    tokenized_seq = self.tokenizer.tokenize(self.sequence,
-                                            is_split_into_words=True,
-                                            padding='max_length',
-                                            truncation=True,
-                                            max_length=self.max_length,
-                                            return_tensors='pt')
+    input_tokens = input_tokens[-self.max_length:]
 
-    # tokenized_sentences.append(tokenized_seq)
+    if len(output_tokens) < self.max_length:
+      diff = self.max_length - len(output_tokens)
+      padding = [self.word_to_indices['<pad>']] * diff
+      output_tokens = padding + output_tokens
 
-    input_ids = tokenized_input["input_ids"].squeeze(0)
-    attention_mask = tokenized_input["attention_mask"].squeeze(0)
-    word_ids = tokenized_input.word_ids()
+    output_tokens = output_tokens[-self.max_length:]
 
-    with torch.no_grad():
-        logits = model(**tokenized_input).logits
+    input_ids = torch.LongTensor(input_tokens)
+    output_ids = torch.LongTensor(output_tokens)
 
-    predicted_token_class_ids = logits.argmax(-1)
-    predicted_tokens_classes = [model.config.id2label[t.item()] for t in predicted_token_class_ids[0]]
-
-    sentence_lang_id = []
-
-    prev_word_idx = None
-    for i, word_idx in enumerate(word_ids):
-        if word_idx is None:
-            continue
-        if word_idx != prev_word_idx:  # Only label the first subword of a token
-            sentence_lang_id.append(predicted_tokens_classes[i])
-        prev_word_idx = word_idx
-
-    # sentences_lang_id.append(sentence_lang_id)
-
-    return {
-        'input_ids': input_ids,
-        'attention_mask': attention_mask}
+    return input_ids, output_ids
